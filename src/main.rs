f6d7d531566005024d3b9d07d8afc7b200358d20
Mathieu Soysal <43273304+MathieuSoysal@users.noreply.github.com>
2024-02-16 07:48:07 +0000

    🎉 init rust project

---
+++ src/main.rs
@@ -0,0 +1,61 @@
+#![feature(test)]
+extern crate test;
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    let i = args
+        .get(1)
+        .expect("Give one argument")
+        .parse::<u32>()
+        .expect("Given argument should be a number.");
+    play_game(i);
+}
+
+pub fn play_game(n: u32) {
+    println!("{}", fizz_buzz_fibonacci(n));
+}
+
+fn is_fibonacci_number(n: u32) -> bool {
+    for i in 0..=n {
+        let (mut previous, mut current) = (0, 1);
+        while current < i {
+            let next = previous + current;
+            previous = current;
+            current = next;
+        }
+        if current == n {
+            return true;
+        }
+    }
+    false
+}
+
+pub fn fizz_buzz_fibonacci(n: u32) -> String {
+    if is_fibonacci_number(n) {
+        "Fibonacci".to_string()
+    } else {
+        match (n % 3, n % 5) {
+            (0, 0) => "FizzBuzz".to_string(),
+            (0, _) => "Fizz".to_string(),
+            (_, 0) => "Buzz".to_string(),
+            (_, _) => n.to_string(),
+        }
+    }
+}
+
+#[cfg(test)]
+mod benchmarks {
+    use test::Bencher;
+
+    use super::play_game;
+    use std::hint::black_box;
+
+    #[bench]
+    fn bench_play_game(b: &mut Bencher) {
+        b.iter(|| {
+            black_box(for i in 1..=100 {
+                play_game(i)
+            });
+        });
+    }
+}
