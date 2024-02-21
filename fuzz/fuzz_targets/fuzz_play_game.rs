4804691b2b81b7197454d5b098f83e9273bd0d40
Mathieu Soysal <43273304+MathieuSoysal@users.noreply.github.com>
2024-02-21 17:41:34 +0000

    ✨ Refactor fuzz_play_game.rs and add lib.rs with fizz_buzz_fibonacci implementation

--- fuzz/fuzz_targets/fuzz_play_game.rs
+++ fuzz/fuzz_targets/fuzz_play_game.rs
@@ -1,13 +1,11 @@
 #![no_main]
 
 use libfuzzer_sys::fuzz_target;
-#[macro_use] extern crate libfuzzer_sys;
+#[macro_use]
+extern crate libfuzzer_sys;
 
-use template_exercisme::play_game;
+use template_exercisme::fizz_buzz_fibonacci;
 
-fuzz_target!(|data: &[u8]| {
-    if let Ok(s) = std::str::from_utf8(data) {
-        let i = s.parse::<u32>().unwrap_or(0);
-        play_game(i);
-    }
+fuzz_target!(|data: u32| {
+        fizz_buzz_fibonacci(data);
 });
